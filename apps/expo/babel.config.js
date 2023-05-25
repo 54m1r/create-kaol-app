e884d87036399ae4300c6b366c7fff9a8192d146
skaraouan <samir.karaouan@justtrade.com>
2023-05-25 13:37:35 +0200

    wip

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -6,6 +6,12 @@ module.exports = function (api) {
       'react-native-reanimated/plugin',
       // https://expo.github.io/router/docs/intro#configure-the-babel-plugin
       require.resolve('expo-router/babel'),
+      //'react-native-web',
+    //   ["module-resolver", {
+    //   "alias": {
+    //     "^react-native$": "react-native-web"
+    //   }
+    // }]
     ],
   }
 }
